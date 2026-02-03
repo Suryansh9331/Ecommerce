da5466a310a685a1d87018b324fa2e4c19258ee7
Suryansh9331 <suryanshmishra9331@gmail.com>
2026-02-03 13:50:28 +0530

    all good

--- src/components/home/Bottom1Carousel.tsx
+++ src/components/home/Bottom1Carousel.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 
 const images = [
+  // "https://res.cloudinary.com/do3vxz4gw/image/upload/v1751544854/svg_assets/bottom1crousel_Image1.svg",
+  // "https://res.cloudinary.com/do3vxz4gw/image/upload/v1751544854/svg_assets/bottom1crousel_Image2.svg",
+  // "https://res.cloudinary.com/do3vxz4gw/image/upload/v1751544854/svg_assets/bottom1crousel_Image3.svg"
   "https://res.cloudinary.com/djimsqy66/image/upload/v1770105245/banner2_xzrfp0.jpg",
   "https://res.cloudinary.com/djimsqy66/image/upload/v1770105246/banner5_y4j38t.jpg",
   "https://res.cloudinary.com/djimsqy66/image/upload/v1770105246/banner4_me34jb.jpg",
@@ -60,7 +63,5 @@ const Bottom1Carousel: React.FC = () => {
 };
 
 export default Bottom1Carousel;
-//backup images links for future
-// "https://res.cloudinary.com/do3vxz4gw/image/upload/v1751544854/svg_assets/bottom1crousel_Image1.svg",
-  // "https://res.cloudinary.com/do3vxz4gw/image/upload/v1751544854/svg_assets/bottom1crousel_Image2.svg",
-  // "https://res.cloudinary.com/do3vxz4gw/image/upload/v1751544854/svg_assets/bottom1crousel_Image3.svg"
+
+
